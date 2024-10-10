42e196208c5afd9a7878846c0767f63c707513ce
Jhonalzate8 <jwalzate08@ciaf.edu.co>
2024-10-09 20:52:37 -0500

    iniciando repo

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "ReservaDevehiculosParcial"
+
---
+++ src/main/java/org/example/ParcialPrograReservaVehiculos.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+class Vehiculo {
+    String idVehiculo;
+    String marca;
+    String modelo;
+    int ano;
+    double costoDiario;
+    boolean disponibilidad;
+}
+
+
+
