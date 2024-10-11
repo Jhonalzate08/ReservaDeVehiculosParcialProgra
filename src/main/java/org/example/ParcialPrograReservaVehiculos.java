c76866df9edc80bfdb5aebad72b9f9af53486b5e
Jhonalzate8 <jwalzate08@ciaf.edu.co>
2024-10-11 17:54:40 -0500

    limpiando la master para agregar el merge de develop con el codigo final

--- src/main/java/org/example/ParcialPrograReservaVehiculos.java
+++ src/main/java/org/example/ParcialPrograReservaVehiculos.java
@@ -4,15 +4,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-class Vehiculo {
-    String idVehiculo;
-    String marca;
-    String modelo;
-    int ano;
-    double costoDiario;
-    boolean disponibilidad;
-
-}
 
 
 
